fecb286a3bf62e90f2fd83f06b32f7093d1925ec
CuongKenn <cuong136205@gmail.com>
2026-08-20 21:45:43 +0700

    chore: wip esms auth

--- backend/server/internal/repository/user_repository.go
+++ backend/server/internal/repository/user_repository.go
@@ -5,13 +5,18 @@ package repository
 
 import (
 	"context"
+	"errors"
 
+	"github.com/Cong-ty-TNNH-Q-Tech/Q-Love/backend/server/internal/models"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type UserRepository interface {
 	GetTopUsersByScore(ctx context.Context, limit int) ([]uuid.UUID, error)
+	Create(ctx context.Context, user *models.User) error
+	FindByPhone(ctx context.Context, phone string) (*models.User, error)
+	FindByID(ctx context.Context, id uuid.UUID) (*models.User, error)
 }
 
 type userRepository struct {
@@ -33,4 +38,32 @@ func (r *userRepository) GetTopUsersByScore(ctx context.Context, limit int) ([]u
 		Pluck("user_id", &userIDs).Error
 		
 	return userIDs, err
+}
+
+func (r *userRepository) Create(ctx context.Context, user *models.User) error {
+	return GetDB(ctx, r.db).WithContext(ctx).Create(user).Error
+}
+
+func (r *userRepository) FindByPhone(ctx context.Context, phone string) (*models.User, error) {
+	var user models.User
+	err := GetDB(ctx, r.db).WithContext(ctx).Where("phone = ?", phone).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil // Return nil if not found instead of error
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) FindByID(ctx context.Context, id uuid.UUID) (*models.User, error) {
+	var user models.User
+	err := GetDB(ctx, r.db).WithContext(ctx).Where("id = ?", id).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
 }
